b468de406e6591fd248485eda58c35cf78663479
Joshua Reetz <thetux2@gmail.com>
2016-03-07 13:49:40 -0700

    First Commit

---
+++ src/com/minecade/minecraftmaker/MinecraftMaker.java
@@ -0,0 +1,17 @@
+package com.minecade.minecraftmaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftMaker extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+}
